19cee3e4421d69e62d1b400ecd27613c45c9cdaa
Mena <dayjacs03@gmail.com>
2025-07-28 18:02:18 -0500

    im going to make it the best way i can maybe wont use much java not sure

--- etchnsketch.js
+++ etchnsketch.js
@@ -1,40 +1,11 @@
-let container = document.querySelector(".container");
+flexContainer = document.querySelector(".flex-container");
+
 
 let x = 0;
 
-    while (x < 256) {
-            x++;
-            let grid = document.createElement("div");
-            container.appendChild(grid);
-            grid.classList.add("grid");
-
-    }
-
-let topcontain = document.querySelector(".topcontain");
-
-let red = document.createElement("button");
-topcontain.appendChild(red);
-red.textContent = "Red";
-
-let blue = document.createElement("button");
-topcontain.appendChild(blue);
-blue.textContent = "Blue";
-
-let black = document.createElement("button");
-topcontain.appendChild(black)
-black.textContent = "Black";
-
-
-red.addEventListener('click', function() {
-    grid.style.backgroundColor = "red";
-});
-
-blue.addEventListener('click',)
-
-
-
-grid.addEventListener('mousedown', function() {
-    if mousedown is true: grid.style.backgroundColor = "users choice"
-}
-
-)
+while (x < 256) {
+        x++;
+        grid = document.createElement("div");
+        flexContainer.appendChild(grid);
+        grid.classList.add("grid");
+};
