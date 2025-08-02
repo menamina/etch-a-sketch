817f3d9af2b456dfe95d2d4df67be5769d0733f7
Mena <dayjacs03@gmail.com>
2025-08-02 14:37:44 -0500

    working on clearing board function

--- etchnsketch.js
+++ etchnsketch.js
@@ -1,21 +1,36 @@
-board = document.querySelector(".board");
+const board = document.querySelector(".board");
+const fixedGrid = 500;
 
-for (let x = 0; x < 256; x++) {
-        grid = document.createElement("div");
-        board.appendChild(grid);
-        grid.classList.add("grid");
-};
+// creating function to use to create divs;
+function createGrid(gridAmount){
+        board.innerHTML = '';
+        board.style.width = `${fixedGrid}px`;
+        board.style.height = `${fixedGrid}px`;
+
+        let gridSize = fixedGrid / gridAmount;
+
+        for(let x = 0; x < gridAmount * gridAmount; x++ ){
+                let grid = document.createElement("div");
+                grid.classList.add("grid");
+                grid.style.width = `${gridSize}px`;
+                grid.style.height = `${gridSize}px`;
+                board.appendChild(grid);
+        }
+}
+
+// calling function to create a 16x16 grid
+createGrid(16);
 
 
 let submit = document.querySelector(".submit");
-submit.addEventListener('click', function(gridnew) {
-        gridnew = Number(document.querySelector(".textbox").value);
-        const fixedGrid = 500;
-        let gridSize = fixedGrid / gridnew;
-
+submit.addEventListener('click', function() {
+        let gridNew = Number(document.querySelector(".textbox").value);
+        if(gridNew > 0 && gridNew <= 100){
+                createGrid(gridNew);
+        }
 })
 
-let selectedColorValue;
+let selectedColorValue = '';
 
 let colorWanted = document.querySelector(".colorWanted");
 colorWanted.addEventListener('click', selectedColor);
@@ -26,16 +41,28 @@ function selectedColor(event){
                 selectedColorValue = color.textContent;
         }
 
+        console.log("Selected color is:", selectedColorValue);
+
 }
 
 
-let parentContainer = document.querySelector(".flex-container");
-parentContainer.addEventListener('click', selectGrid);
+let parentContainer = document.querySelector(".board");
+parentContainer.addEventListener('mousedown', selectGrid);
 
 function selectGrid(event){
         let individualGridColor = event.target;
         if(individualGridColor.classList.contains("grid")){
-                individualGridColor.style.background = selectedColorValue;
+                individualGridColor.style.backgroundColor = selectedColorValue;
         }
 
+        console.log("You clicked a grid")
+
+}
+
+let clear = document.querySelector(".clear");
+clear.addEventListener('click', clearDaBoard);
+
+function clearDaBoard(event){
+        erase = event.target;
+        
 }
