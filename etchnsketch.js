865b86c8cf4682cb603af8c3af961097222b1c6c
Mena <dayjacs03@gmail.com>
2025-08-02 13:21:51 -0500

    wip

--- etchnsketch.js
+++ etchnsketch.js
@@ -1,11 +1,41 @@
-flexContainer = document.querySelector(".flex-container");
+board = document.querySelector(".board");
 
-
-let x = 0;
-
-while (x < 256) {
-        x++;
+for (let x = 0; x < 256; x++) {
         grid = document.createElement("div");
-        flexContainer.appendChild(grid);
+        board.appendChild(grid);
         grid.classList.add("grid");
 };
+
+
+let submit = document.querySelector(".submit");
+submit.addEventListener('click', function(gridnew) {
+        gridnew = Number(document.querySelector(".textbox").value);
+        const fixedGrid = 500;
+        let gridSize = fixedGrid / gridnew;
+
+})
+
+let selectedColorValue;
+
+let colorWanted = document.querySelector(".colorWanted");
+colorWanted.addEventListener('click', selectedColor);
+
+function selectedColor(event){
+        let color = event.target;
+        if(color.classList.contains("color")){
+                selectedColorValue = color.textContent;
+        }
+
+}
+
+
+let parentContainer = document.querySelector(".flex-container");
+parentContainer.addEventListener('click', selectGrid);
+
+function selectGrid(event){
+        let individualGridColor = event.target;
+        if(individualGridColor.classList.contains("grid")){
+                individualGridColor.style.background = selectedColorValue;
+        }
+
+}
